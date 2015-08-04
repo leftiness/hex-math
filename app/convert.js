c2a1e9940294f2614778609adfcc0e769118112e
Brandon Parmenter <leftiness@gmail.com>
2015-08-04 18:58:49 -0400

    Refactored project for modularity

---
+++ app/convert.js
@@ -0,0 +1,116 @@
+var convert = {};
+
+convert.from = {};
+
+convert.from.axial = {};
+convert.from.axial.to = {};
+
+convert.from.cube = {};
+convert.from.cube.to = {};
+
+convert.from.axial.to.cube = function (axial, keys) {
+  var qKey;
+  var rKey;
+  var q;
+  var r;
+  var outcome;
+  var emptyAxial;
+  var emptyKeys;
+  var axialIsArray;
+
+  emptyAxial = !!!axial || axial.length === 0;
+
+  if(emptyAxial) {
+    throw new Error("Invalid empty coordinates. " + axial);
+  }
+
+  axialIsArray = Array.isArray(axial);
+  emptyKeys = !!!keys || keys.length === 0;
+
+  if (!axialIsArray && emptyKeys) {
+    throw new Error("Keys are required when coordinates are in an object. " + keys);
+  }
+
+  keys = keys || [0, 1];
+  keys = keys.length === 0 ? [0, 1] : keys;
+
+  qKey = keys[0];
+  rKey = keys[1];
+
+  q = axial[qKey];
+  r = axial[rKey];
+
+  if (typeof q !== 'number' || typeof r !== 'number') {
+    throw new Error('Invalid coordinates. Type is not a number. ' + [q, r]);
+  }
+
+  if (axialIsArray) {
+    outcome = [q, -q - r, r];
+  } else {
+    outcome = {
+      'x': q,
+      'y': -q - r,
+      'z': r
+    };
+  }
+
+  return outcome;
+};
+
+convert.from.cube.to.axial = function (cube, keys) {
+  var xKey;
+  var yKey;
+  var zKey;
+  var x;
+  var y;
+  var z;
+  var outcome;
+  var emptyCube;
+  var emptyKeys;
+  var cubeIsArray;
+
+  emptyCube = !!!cube || cube.length === 0;
+
+  if (emptyCube) {
+    throw new Error('Invalid empty coordinates. ' + cube);
+  }
+
+  cubeIsArray = Array.isArray(cube);
+  emptykeys = !!!keys || keys.length === 0;
+
+  if (!cubeIsArray && emptyKeys) {
+    throw new Error('Keys are required when coordinates are in an object. ' + keys);
+  }
+
+  keys = keys || [0, 1, 2];
+  keys = keys.length === 0 ? [0, 1, 2] : keys;
+
+  xKey = keys[0];
+  yKey = keys[1];
+  zKey = keys[2];
+
+  x = cube[xKey];
+  y = cube[yKey];
+  z = cube[zKey];
+
+  if (typeof x !== 'number' || typeof y !== 'number' || typeof z !== 'number') {
+    throw new Error('Invalid coordinates. Type is not number. ' + [x, y, z]);
+  }
+
+  if (x + y + z !== 0) {
+    throw new Error('Invalid coordinates. X + Y + Z should equal 0. ' + [x, y, z]);
+  }
+
+  if (cubeIsArray) {
+    outcome = [x, z];
+  } else {
+    outcome = {
+      'q': x,
+      'r': z
+    };
+  }
+
+  return outcome;
+};
+
+module.exports = convert;
---
+++ test/hex-math.spec.js
@@ -0,0 +1,18 @@
+var should = require('chai').should();
+var hex = require('../hex-math.js');
+
+describe('hex', function () {
+
+  it('should exist', function () {
+      hex.should.exist;
+  });
+
+  describe('convert', function () {
+
+    it('should exist', function () {
+      hex.convert.should.exist;
+    });
+
+  });
+
+});
