b833ad12c64acedf7361f201fb5b8b3a35d13cd2
Brandon Parmenter <leftiness@gmail.com>
2015-08-09 19:37:15 -0400

    Refactored factory methods into one generic method

--- app/factory.js
+++ app/factory.js
@@ -2,82 +2,42 @@ var _ = require('lodash');
 
 var _factory = {};
 
-_.set(_factory, 'create.from.axial', function (input) {
-  var q;
-  var r;
-  var outcome;
+var _create = function (input, keys) {
+  var output = {};
+  var array = [];
 
   if (_.isEmpty(input)) {
     throw new Error('Coordinates must be array or object. ' + input);
   };
 
-  if (_.isArray(input)) {
-    q = input[0];
-    r = input[1];
-  } else {
-    q = input['q'];
-    r = input['r'];
+  for (i = 0; i < keys.length; i++) {
+    var coord = _.isArray(input) ? input[i] : input[keys[i]];
+
+    if (!_.isNumber(coord)) {
+      throw new Error('Coordinate type is not a number. '
+        + keys[i]
+        + ':'
+        + coord
+      );
+    }
+
+    output[keys[i]] = coord;
+    array[i] = coord;
   }
 
-  ([q, r]).forEach(function (each) {
-    if (!_.isNumber(each)) {
-        throw new Error('Coordinate type is not a number. ' + each);
-    };
+  _.set(output, 'to.array', function () {
+    return array;
   });
 
-  outcome = {
-    'q': q,
-    'r': r
-  };
+  return output;
+}
 
-  _.set(outcome, 'to.array', function () {
-    return [q, r];
-  });
-
-  return outcome;
+_.set(_factory, 'create.from.axial', function (input) {
+  return _create(input, ['q', 'r']);
 });
 
 _.set(_factory, 'create.from.cube', function (input) {
-  var x;
-  var y;
-  var z;
-  var outcome;
-
-  if (_.isEmpty(input)) {
-    throw new Error('Coordinates must be array or object. ' + input);
-  };
-
-  if (_.isArray(input)) {
-    x = input[0];
-    y = input[1];
-    z = input[2];
-  } else {
-    x = input['x'];
-    y = input['y'];
-    z = input['z'];
-  }
-
-  ([x, y, z]).forEach(function (each) {
-    if (!_.isNumber(each)) {
-        throw new Error('Coordinate type is not a number. ' + each);
-    };
-  });
-
-  if (x + y + z !== 0) {
-    throw new Error('X + Y + Z should equal 0. ' + [x, y, z]);
-  }
-
-  outcome = {
-    'x': x,
-    'y': y,
-    'z': z
-  };
-
-  _.set(outcome, 'to.array', function () {
-    return [x, y, z];
-  });
-
-  return outcome;
+  return _create(input, ['x', 'y', 'z']);
 });
 
 module.exports = _factory;
