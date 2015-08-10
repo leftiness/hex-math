d245740aab77973321d365d1f8f3938c28972564
Brandon Parmenter <leftiness@gmail.com>
2015-08-09 21:27:11 -0400

    Missed h coordinate in convert

--- app/convert.js
+++ app/convert.js
@@ -7,16 +7,18 @@ _.set(_convert, 'from.axial.to.cube', function (axial) {
   var hex = _factory.create.from.axial(axial);
   var q = hex.q;
   var r = hex.r;
+  var h = hex.h;
 
-  return _factory.create.from.cube([q, -q - r, r]);
+  return _factory.create.from.cube([q, -q - r, r, h]);
 });
 
 _.set(_convert, 'from.cube.to.axial', function (cube) {
   var hex = _factory.create.from.cube(cube);
   var x = hex.x;
   var z = hex.z;
+  var h = hex.h;
 
-  return _factory.create.from.axial([x, z]);
+  return _factory.create.from.axial([x, z, h]);
 });
 
 module.exports = function (factory) {
