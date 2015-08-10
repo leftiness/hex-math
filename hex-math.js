797a4025724a12022fe66a45b5bf00e5da172710
Brandon Parmenter <leftiness@gmail.com>
2015-08-09 21:32:37 -0400

    Fixed dependencies

--- hex-math.js
+++ hex-math.js
@@ -1,11 +1,13 @@
 var _hex = {}
 
 var _factory = require('./app/factory.js');
-var _convert = require('./app/convert.js');
-var _neighbors = require('./app/neighbors.js');
+var _convert = require('./app/convert.js')(_factory);
+var _neighbors = require('./app/neighbors.js')(_factory, _convert);
+var _distance = require('./app/distance.js')(_factory, _convert);
 
 _hex.factory = _factory;
-_hex.convert = _convert(_factory);
-_hex.neighbors = _neighbors(_factory, _convert);
+_hex.convert = _convert;
+_hex.neighbors = _neighbors;
+_hex.distance = _distance;
 
 module.exports = _hex;
