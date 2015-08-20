ad16397956524fbf10987e9ba3fe5b5640ff2ebb
Brandon Parmenter <leftiness@gmail.com>
2015-08-19 20:26:38 -0400

    Updated neighbors module
    - Separate functions for lateral neighbors and neighbors with height

--- app/neighbors.js
+++ app/neighbors.js
@@ -15,21 +15,40 @@ var _directions = [
 
 _.set(_nei, 'from.axial', function(axial) {
   var output = [];
-  var heights = [-1, 0, 1];
 
   axial = _factory.create.from.axial(axial);
 
-  heights.forEach(function (height) {
-    _directions.forEach(function (direction) {
-      var hex = {};
+  _directions.forEach(function (direction) {
+    var q = axial.q + direction[0];
+    var r = axial.r + direction[1];
+    var h = axial.h;
+    var hex = _factory.create.from.axial([q, r, h]);
 
-      hex.q = axial.q + direction[0];
-      hex.r = axial.r + direction[1];
-      hex.h = axial.h + height;
-
-      output.push(hex);
-    })
+    output.push(hex);
   });
+
+  return output;
+});
+
+_.set(_nei, 'from.axial.with.height', function (axial) {
+  var output = [];
+  var above;
+  var below;
+  var results;
+
+  axial = _factory.create.from.axial(axial);
+
+  above = _.clone(axial);
+  below = _.clone(axial);
+
+  above.h += 1;
+  below.h -= 1;
+
+  results = _nei.from.axial(axial);
+
+  output = output.concat(results);
+  output.push(above);
+  output.push(below);
 
   return output;
 });
@@ -37,6 +56,18 @@ _.set(_nei, 'from.axial', function(axial) {
 _.set(_nei, 'from.cube', function(cube) {
   var axial = _convert.from.cube.to.axial(cube);
   var results = _nei.from.axial(axial);
+  var output = [];
+
+  results.forEach(function (result) {
+    output.push(_convert.from.axial.to.cube(result));
+  });
+
+  return output;
+});
+
+_.set(_nei, 'from.cube.with.height', function (cube) {
+  var axial = _convert.from.cube.to.axial(cube);
+  var results = _nei.from.axial.with.height(axial);
   var output = [];
 
   results.forEach(function (result) {
--- test/app/neighbors.spec.js
+++ test/app/neighbors.spec.js
@@ -4,9 +4,6 @@ var _ = require('lodash');
 
 var _nei = require('./../../app/neighbors.js');
 
-var _factory = {};
-var _convert = {};
-
 var _axial = {
   'q': 2,
   'r': -3,
@@ -20,13 +17,31 @@ var _cube = {
   'h': 1
 };
 
+var _Hex = function (coords) {
+  if (_.isArray(coords)) {
+      var output = {};
+
+      output['q'] = coords[0];
+      output['r'] = coords.length === 3 ? coords[1] : coords[2];
+      output['x'] = coords[0];
+      output['z'] = coords.length === 3 ? coords[1] : coords[2];
+      output['y'] = -output['x'] - output['z'];
+      output['h'] = coords.length === 3 ? coords[2] : coords[3];
+
+      return output;
+  } else {
+    coords.x = coords.x || coords.q;
+    coords.z = coords.z || coords.r;
+    coords.y = -coords.x - coords.z;
+    coords.q = coords.q || coords.x;
+    coords.r = coords.r || coords.z;
+
+    return coords;
+  }
+};
+
 var _arrays = [
-  [3, 0, -3, 0],
-  [3, 1, -4, 0],
-  [2, 2, -4, 0],
-  [1, 2, -3, 0],
-  [1, 1, -2, 0],
-  [2, 0, -2, 0],
+  [2, 1, -3, 0],
 
   [3, 0, -3, 1],
   [3, 1, -4, 1],
@@ -35,70 +50,167 @@ var _arrays = [
   [1, 1, -2, 1],
   [2, 0, -2, 1],
 
-  [3, 0, -3, 2],
-  [3, 1, -4, 2],
-  [2, 2, -4, 2],
-  [1, 2, -3, 2],
-  [1, 1, -2, 2],
-  [2, 0, -2, 2]
+  [2, 1, -3, 2]
 ];
 
-var stubAxial = sinon.stub();
-var stubCube = sinon.stub();
+var _hexes = [];
 
-for (i = 0; i < _arrays.length; i++) {
-  var hex = {};
-
-  hex.q = _arrays[i][0];
-  hex.r = _arrays[i][2];
-  hex.x = _arrays[i][0];
-  hex.y = _arrays[i][1];
-  hex.z = _arrays[i][2];
-  hex.h = _arrays[i][3];
-
-  stubAxial.onCall(i).returns(hex);
-  stubCube.onCall(i).returns(hex);
-}
-
-_.set(_factory, 'create.from.axial', sinon.stub().returns(_axial));
-_.set(_convert, 'from.cube.to.axial', stubAxial);
-_.set(_convert, 'from.axial.to.cube', stubCube);
-
-_nei = _nei(_factory, _convert);
+_arrays.forEach(function (ar) {
+  _hexes.push(_Hex(ar));
+});
 
 describe('neighbors.from.cube()', function () {
   it('should exist', function () {
-    _nei.from.cube.should.exist;
+    _nei().from.cube.should.exist;
   });
 
-  it('should return an array of cube coordinate objects', function () {
-    var outcome = _nei.from.cube(_cube);
+  it('should return the array of hexes', function () {
+    var factory = {};
+    var convert = {};
+    var nei;
+    var results;
 
-    outcome.should.not.be.empty;
+    _.set(convert, 'from.cube.to.axial', _Hex);
+    _.set(convert, 'from.axial.to.cube', _Hex);
 
-    _arrays.forEach(function (array, i) {
-      outcome[i].should.have.property('x').which.eql(array[0]);
-      outcome[i].should.have.property('y').which.eql(array[1]);
-      outcome[i].should.have.property('z').which.eql(array[2]);
-      outcome[i].should.have.property('h').which.eql(array[3]);
+    _.set(factory, 'create.from.axial', _Hex);
+    _.set(factory, 'create.from.cube', _Hex);
+
+    nei = _nei(factory, convert);
+
+    results = nei.from.cube(_cube);
+
+    _.filter(_hexes, function (hex) {
+      return hex.h === 1;
+    }).forEach(function (hex) {
+      var filter = _.filter(results, function (result) {
+        var x = result.x === hex.x;
+        var y = result.y === hex.y;
+        var z = result.z === hex.z;
+        var h = result.h === hex.h;
+
+        return _.every([x, y, z, h]);
+      });
+
+      hex.should.have.property('x').which.eql(_.get(filter, '[0].x'));
+      hex.should.have.property('y').which.eql(_.get(filter, '[0].y'));
+      hex.should.have.property('z').which.eql(_.get(filter, '[0].z'));
+      hex.should.have.property('h').which.eql(_.get(filter, '[0].h'));
+    });
+  });
+});
+
+describe('neighbors.from.cube.with.height()', function () {
+  it('should exist', function () {
+    _nei().from.cube.with.height.should.exist;
+  });
+
+  it('should return the array of hexes', function () {
+    var factory = {};
+    var convert = {};
+    var nei;
+    var results;
+
+    _.set(convert, 'from.cube.to.axial', _Hex);
+    _.set(convert, 'from.axial.to.cube', _Hex);
+
+    _.set(factory, 'create.from.axial', _Hex);
+    _.set(factory, 'create.from.cube', _Hex);
+
+    nei = _nei(factory, convert);
+
+    results = nei.from.cube.with.height(_cube);
+
+    _hexes.forEach(function (hex) {
+      var filter = _.filter(results, function (result) {
+        var x = result.x === hex.x;
+        var y = result.y === hex.y;
+        var z = result.z === hex.z;
+        var h = result.h === hex.h;
+
+        return _.every([x, y, z, h]);
+      });
+
+      hex.should.have.property('x').which.eql(_.get(filter, '[0].x'));
+      hex.should.have.property('y').which.eql(_.get(filter, '[0].y'));
+      hex.should.have.property('z').which.eql(_.get(filter, '[0].z'));
+      hex.should.have.property('h').which.eql(_.get(filter, '[0].h'));
     });
   });
 });
 
 describe('neighbors.from.axial()', function () {
   it('should exist', function () {
-    _nei.from.axial.should.exist;
+    _nei().from.axial.should.exist;
   });
 
-  it('should return an array of axial coordinate objects', function () {
-    var outcome = _nei.from.axial(_axial);
+  it('should return the array of hexes', function () {
+    var factory = {};
+    var convert = {};
+    var nei;
+    var results;
 
-    outcome.should.not.be.empty;
+    _.set(convert, 'from.cube.to.axial', _Hex);
+    _.set(convert, 'from.axial.to.cube', _Hex);
 
-    _arrays.forEach(function (array, i) {
-      outcome[i].should.have.property('q').which.eql(array[0]);
-      outcome[i].should.have.property('r').which.eql(array[2]);
-      outcome[i].should.have.property('h').which.eql(array[3]);
+    _.set(factory, 'create.from.axial', _Hex);
+    _.set(factory, 'create.from.cube', _Hex);
+
+    nei = _nei(factory, convert);
+
+    results = nei.from.axial(_axial);
+
+    _.filter(_hexes, function (hex) {
+      return hex.h === 1;
+    }).forEach(function (hex) {
+      var filter = _.filter(results, function (result) {
+        var q = result.q === hex.q;
+        var r = result.r === hex.r;
+        var h = result.h === hex.h;
+
+        return _.every([q, r, h]);
+      });
+
+      hex.should.have.property('q').which.eql(_.get(filter, '[0].q'));
+      hex.should.have.property('r').which.eql(_.get(filter, '[0].r'));
+      hex.should.have.property('h').which.eql(_.get(filter, '[0].h'));
+    });
+  });
+});
+
+describe('neighbors.from.axial.with.height()', function () {
+  it('should exist', function () {
+    _nei().from.axial.with.height.should.exist;
+  });
+
+  it('should return the array of hexes', function () {
+    var factory = {};
+    var convert = {};
+    var nei;
+    var results;
+
+    _.set(convert, 'from.cube.to.axial', _Hex);
+    _.set(convert, 'from.axial.to.cube', _Hex);
+
+    _.set(factory, 'create.from.axial', _Hex);
+    _.set(factory, 'create.from.cube', _Hex);
+
+    nei = _nei(factory, convert);
+
+    results = nei.from.axial.with.height(_axial);
+
+    _hexes.forEach(function (hex) {
+      var filter = _.filter(results, function (result) {
+        var q = result.q === hex.q;
+        var r = result.r === hex.r;
+        var h = result.h === hex.h;
+
+        return _.every([q, r, h]);
+      });
+
+      hex.should.have.property('q').which.eql(_.get(filter, '[0].q'));
+      hex.should.have.property('r').which.eql(_.get(filter, '[0].r'));
+      hex.should.have.property('h').which.eql(_.get(filter, '[0].h'));
     });
   });
 });
